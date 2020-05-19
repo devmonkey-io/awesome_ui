db0d145b817f2f087a06b10570409a7913b41668
DevMonkey Inc <devmonkey.in@gmail.com>
2020-05-19 18:16:46 +0530

    added homescreen

---
+++ lib/awesome_ui_app.dart
@@ -0,0 +1,13 @@
+import 'package:awesome_ui/screen/home.dart';
+import 'package:flutter/material.dart';
+
+class AwesomeUIApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Awesome UI',
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:awesome_ui/app.dart';
-import 'package:awesome_ui/screen/home.dart';
+import 'package:awesome_ui/awesome_ui_app.dart';
 
 void main() {
-  String appTitle = 'Awesome UI';
-  Text myTextWidget = Text('awesome ui project');
-  Text homeScreenTitle = Text('Home Screen');
-  ThemeData awesomeUiThemeData = ThemeData(
-    brightness: Brightness.light,
-//    primarySwatch: Colors.orange,
-    primaryColor: Colors.white,
-  );
-
-  Text fabChild = Text('Add');
-  Icon fabChildIcon = Icon(Icons.add);
-
-  FloatingActionButton homeScreenFAB = FloatingActionButton(
-    backgroundColor: Colors.orange,
-    child: fabChildIcon,
-    onPressed: pressMe,
-  );
-
-  TextStyle someBodyTextStyle = TextStyle(
-    fontSize: 32.0,
-    fontWeight: FontWeight.bold,
-  );
-
-  Text someBodyText = Text(
-    'Body content goes here',
-    style: someBodyTextStyle,
-  );
-
-  AppBar homeScreenAppBar = AppBar(title: homeScreenTitle);
-
-  Scaffold homeScreen = Scaffold(
-    appBar: homeScreenAppBar,
-    floatingActionButton: homeScreenFAB,
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    body: Center(
-      child: someBodyText,
-    ),
-  );
-
-  MaterialApp awesomeUIApp = MaterialApp(
-    title: appTitle,
-    home: homeScreen,
-    theme: awesomeUiThemeData,
-    debugShowCheckedModeBanner: false,
-  );
-
-  runApp(awesomeUIApp);
+  runApp(AwesomeUIApp());
 }
 
-void pressMe() {
-  print('i got pressed');
-}
