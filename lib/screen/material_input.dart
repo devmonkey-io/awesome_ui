aa311ffc8daebd33007670742c19272e5d1a7318
DevMonkey Inc <devmonkey.in@gmail.com>
2020-05-27 17:35:09 +0530

    added material input screen

--- lib/screen/material_input.dart
+++ lib/screen/material_input.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class MaterialInputScreen extends StatelessWidget {
+  final List<Widget> _children = <Widget>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Material Inputs and Selections'),
+      ),
+      body: ListView(
+        children: _children,
       ),
     );
   }
