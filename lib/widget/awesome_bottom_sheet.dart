70c7240935bac08fbc45c415a634aa9b0da3387d
DevMonkey Inc <devmonkey.in@gmail.com>
2020-05-29 16:57:50 +0530

    added bottom sheet

--- lib/widget/awesome_bottom_sheet.dart
+++ lib/widget/awesome_bottom_sheet.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
 
 class AwesomeBottomSheetWidget extends StatelessWidget {
+  BuildContext _buildContext;
+
   @override
   Widget build(BuildContext context) {
+    _buildContext = context;
+
     return ListTile(
-      title: Text('Tap to open bottom sheet'),
-      trailing: Icon(Icons.vertical_align_bottom),
+      title: Text('Tap to report this content'),
+      trailing: Icon(
+        Icons.bug_report,
+        color: Colors.red,
+      ),
       onTap: _onTap,
     );
   }
 
-  void _onTap() {}
+  void _onTap() {
+    showBottomSheet(
+      context: _buildContext,
+      builder: _bottomSheetBuilder,
+    );
+  }
+
+  Widget _bottomSheetBuilder(BuildContext buildContext) {
+    return Container(
+      color: Colors.grey[200],
+      child: Wrap(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.of(_buildContext).pop();
+            },
+          ),
+          FlatButton(
+            child: Text('Violence'),
+            onPressed: () {},
+          ),
+          FlatButton(
+            child: Text('Racism'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
 }
